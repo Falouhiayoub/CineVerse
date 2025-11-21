5486372a31916e2aab0a792162fffab4e9538e08
Falouhiayoub <falouhiayoub9@gmail.com>
2025-11-21 15:05:00 +0100

    add PopUp

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -5,7 +5,7 @@ const PopUp = () => {
         <div className="welcome-popup-overlay" id="welcomeOverlay">
             <div className="welcome-popup">
                 <h2>Welcome to CineVerse 🎬</h2>
-                <p>Your gateway to the world of amazing movies!</p>
+                <p>Your Gateway to the World of amazing movies!</p>
                 <button id="closePopup">Enter Website</button>
             </div>
         </div>
